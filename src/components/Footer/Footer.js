58d471da1cea81925a737133d2289f1f3e6e1370
Quashnock <awq@uw.edu>
2025-09-03 07:34:18 -0700

    Created basic footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,32 @@
 import './Footer.scss'
+import { links } from './Links'
+
+const date = new Date();
 
 export const Footer = () => {
     return (
-        <footer className='footer'>
-            <h1>Footer</h1>
+        <footer className='footer' role="contentinfo">
+            <div className='gradient'></div>
+            <div className='max-width-container'>
+                <nav aria-label='Social media links' className='social-nav'>
+                    <ul className='social-list'>
+                        {links.map(link => (
+                            <li key={link.url}>
+                                <a 
+                                className='social-link'
+                                href={link.url}
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                    <img src={link.img} alt={link.alt}/>
+                                </a>
+                            </li>
+                            )
+                        )}
+                    </ul>
+                </nav>
+                
+                <p className='copyright'>Copyright © {date.getFullYear()} TrickFire Robotics. All rights reserved. Bothell, WA.</p>
+            </div>
         </footer>
     )
 }
