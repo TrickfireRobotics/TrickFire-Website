ebd2c18e5b7523da91ca6f48be44720c63cf2ccd
Quashnock <awq@uw.edu>
2025-09-03 10:22:08 -0700

    Updated MaxWidthContainer documentation to include a note about its classname.

--- src/components/MaxWidthContainer/MaxWidthContainer.js
+++ src/components/MaxWidthContainer/MaxWidthContainer.js
@@ -3,6 +3,9 @@ import './MaxWidthContainer.scss'
 /**
  * Container component that reduces the max width of its children to 1440px. This is
  * done so that content is displayed in the middle of the screen for ultrawide montiors.
+ * 
+ * Note: the className of this component is "max-width-container". Use this classname when needing
+ * to add additional SCSS styles to this wrapper.
  * @component 
  */
 export const MaxWidthContainer = ({ children }) => <div className='max-width-container'>{children}</div>;
