8d3042b64321dbb26e27456e978b292c8cde8b25
E11aW <Ellaraindeer2022@gmail.com>
2025-09-03 12:09:02 -0700

    Started the skeleton for repeating text background

---
+++ src/components/RepeatingTextBackground/RepeatingTextBackground.js
@@ -0,0 +1,15 @@
+import './RepeatingTextBackground.scss'
+
+/**
+ * Repeating Text Background component that renders the background and box.
+ * @component
+ */
+export const RepeatingTextBackground = () => {
+    return (
+        <header className='repeating-text-background'>
+            <MaxWidthContainer>
+
+            </MaxWidthContainer>
+        </header>
+    )
+}
