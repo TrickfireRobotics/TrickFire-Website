471f70b3ce4f575cfd85e96e23a2d84d89517373
AbbyHoyt <abby.hoyt@comcast.net>
2025-09-07 15:31:25 -0700

    Made non-subpage links open in a new tab for Button components.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const Button = (props) => {
     return (        
         <div class="button-component">
-            {props.isLink && <button><a href={props.link}>{props.buttonText} →</a></button>}
+            {props.isLink && <button><a href={props.link} target="_blank" rel="noreferrer">{props.buttonText} →</a></button>}
             {props.isSubpageLink && <button><Link to={props.link}>{props.buttonText} →</Link></button>}
         </div>
     )
