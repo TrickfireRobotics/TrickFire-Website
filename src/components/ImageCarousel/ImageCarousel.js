2afd07ddca4e8abe4fa4f9165bede0c001c10042
AbbyHoyt <abby.hoyt@comcast.net>
2025-09-12 22:53:29 -0700

    Image carousel is now functional.

--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -3,22 +3,37 @@ import { MaxWidthContainer } from '../MaxWidthContainer/MaxWidthContainer'
 import LeftArrow from '../../assets/AboutUs/leftArrow.png'
 import RightArrow from '../../assets/AboutUs/rightArrow.png'
 
+import { useCallback, useState } from "react";
+
 export const ImageCarousel = (props) => {
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const length = 3
+
+    const nextSlide = () => {
+        setCurrentSlide(currentSlide - 1 < 0 ? length - 1 : currentSlide - 1);
+    };
+    
+    const previousSlide = useCallback(() => {
+        setCurrentSlide(currentSlide + 1 > length - 1 ? 0 : currentSlide + 1);
+    }, [currentSlide, length]);
+
     return (
         <div class="image-carousel">
             <MaxWidthContainer>
                 <h1 class="large-center-text">Our Story</h1>
 
                 <div class="image-and-arrows">
-                    <img class="arrow-button-left" src={LeftArrow} alt="Arrow pointing left."/>
+                    <img class="arrow-button-left" src={LeftArrow} alt="Arrow pointing left." onClick={previousSlide}/>
 
-                    {props.images.map((image) =>
-                        <div class="image-and-overlay" key={image.id}>
-                            <img key={image.id} src={image.src} alt={image.alt} draggable="false"/>
-                        </div>
-                    )}
+                    {props.images.map((image, index) => {
+                        return (
+                            <div key={index}>
+                                {currentSlide === index && <img src={image.src} alt={image.alt} draggable="false"/>}
+                            </div>
+                        );
+                    })}
 
-                    <img class="arrow-button-right" src={RightArrow} alt="Arrow pointing right."/>
+                    <img class="arrow-button-right" src={RightArrow} alt="Arrow pointing right." onClick={nextSlide}/>
                 </div>
 
                 <div class="overlay-text">
