3f5dc9c0c52e52d419cde4e50d68d711a4cf7bbf
Quashnock <awq@uw.edu>
2026-01-06 23:07:15 -0800

    Resolved merge conflict

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,17 +10,10 @@ import { MaxWidthContainer } from '../MaxWidthContainer/MaxWidthContainer.js'
 export const Hero = () => {
     return (
         <div className='hero'>
-<<<<<<< HEAD
-            <img src={roverOnRock} className='background-image' alt="Rover background"/>
-
-            <MaxWidthContainer>
-                <img src={watermelonDragon} className='watermelonDragon' alt="Watermelon dragon mascot"/>
-=======
             <img src={roverOnRock} alt="Viator rover" className='background-image'/>
 
             <MaxWidthContainer>
                 <img src={watermelonDragon} alt="Watermelon dragon mascot" className='watermelonDragon'/>
->>>>>>> f903ae583ab2bb1b5eaa766b58a8d6831b042f69
                 <div className='text'>
                     <h1 className='title'>TrickFire Robotics</h1>
                     <p className='subtitle'>Developing future engineers, developers, and marketers in the world of competitive robotics</p>
